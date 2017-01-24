ff73e10d4d72e9d782f56040a0bb61b0569f17bf
plamen1982 <plamen.hristov@ymail.com>
2017-01-23 22:30:01 -0500

    Added the types of the data in the Class Shape

--- Typescript.js
+++ Typescript.js
@@ -1,15 +1,20 @@
-//Create class Shape with id, x and y coodrinates and method move
+//Create class Shape with id, x and y coordinates and method move
 var Shape = (function () {
     function Shape(id, x, y) {
         this.id = id;
         this.move(x, y);
     }
     Shape.prototype.move = function (x, y) {
+        this.handleError(this.x, this.y);
         this.x = x;
         this.y = y;
         return ("This is your x coordinate: " + this.x + "/n This is your y coordinate: " + this.y);
     };
+    Shape.prototype.handleError = function (x, y) {
+        if (!x || !y) {
+            return console.log('Place both coordinates');
+        }
+    };
     return Shape;
 }());
-var shape = new Shape(1, 15, 20);
-shape.move(20, 25);
+var shape = new Shape(1, 5, 10);
--- Typescript.ts
+++ Typescript.ts
@@ -1,17 +1,23 @@
 //Create class Shape with id, x and y coordinates and method move
 
 class Shape {
-    id
-    x
-    y
-    constructor(id, x, y){
+    id :number
+    x :number
+    y :number
+    constructor(id :number, x :number, y :number){
         this.id = id
         this.move(x, y)
     }
-    move(x, y){
+    move(x :number, y :number){
         this.x = x
         this.y = y
         return("This is your x coordinate: " + this.x + "/n This is your y coordinate: " + this.y)
     }
+    handleError(x :number, y :number){
+        if(!x || !y)  {
+            return console.log('Place both coordinates')
+        }
+    }
 }
 
+let shape = new Shape(1, 5, 10)
